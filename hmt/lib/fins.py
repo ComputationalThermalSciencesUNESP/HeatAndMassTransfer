fc853f558da43429407c4bd73eb4d9b54768aca6
Iago Lessa de Oliveira <iago.lessa.oliveira@gmail.com>
2023-10-01 12:15:11 -0300

    Added piniform triangular and parabolic fins; restructured code to match computation of heat transfer based on efficiency for non-uniform section fins.

--- hmt/lib/fins.py
+++ hmt/lib/fins.py
@@ -29,7 +29,13 @@ import numpy as np
 from typing import Union
 
 # Import modified Bessel functions
-from scipy.special import i0, i1, k0, k1
+from scipy.special import i0, i1, k0, k1, iv
+
+_SMALL = 1.0e-4
+
+def i2(x):
+    """Modified Bessel function of first kind of second order."""
+    return iv(2, x)
 
 # def temperatureFinInfLength(
 #         x: float,
@@ -144,19 +150,8 @@ class Fin():
 
         return self._volume
 
-    def getEfficiency(self):
-        """Compute efficiency of a fin."""
-
-        qMax = self._h*self._area_fin*self._thetaB
-
-        return self.getHeatTransfer()/qMax
-
-    def getEffectiveness(self):
-        """Return fin's effectiveness."""
-
-        qBaseNoFin = self._h*self._area_base*self._thetaB
-
-        return self.getHeatTransfer()/qBaseNoFin
+    def getMaxHeatTransfer(self):
+        return self._h*self._area_fin*self._thetaB
 
     def getThermalResistance(self):
 
@@ -236,6 +231,18 @@ class FinUniformAtr(Fin):
         # by the equation for the adiabatic tip
         return self._M*np.tanh(self._m*self._Lc)
 
+    def getEfficiency(self):
+        """Compute efficiency of a fin."""
+
+        return self.getHeatTransfer()/self.getMaxHeatTransfer()
+
+    def getEffectiveness(self):
+        """Return fin's effectiveness."""
+
+        qBaseNoFin = self._h*self._area_base*self._thetaB
+
+        return self.getHeatTransfer()/qBaseNoFin
+
 class FinNonUniformAtr(Fin):
 
     def __init__(
@@ -289,14 +296,39 @@ class FinNonUniformAtr(Fin):
         x value of rectangular or piniform fins or a radius for annular fins.
         """
         # Must be in derived class for non-uniform fins
-        pass
+        raise NotImplementedError(
+                "You must implement a getTemperature() down the derived class."
+            )
 
     def getHeatTransfer(self):
-        """Return fin's total return transfer."""
-        # Must be in derived classes for non uniform fins
-        pass
+        """Return fin's total heat transfer.
 
-class FinRectangularPlanar(FinUniformAtr):
+        For non-uniform section fins, the heat transfer is computed based on
+        the efficiency, with the exception of annular fins for which the heat
+        transfer was computed explicitly.
+        """
+
+        # For fins that only the efficiency is available, compute heat transfer
+        # based on it
+
+        return self.getMaxHeatTransfer()*self.getEfficiency()
+
+    def getEffectiveness(self):
+        """Return fin's effectiveness.
+
+        For non-uniform section fins, the effectiviness is computed based on
+        the efficiency, with the exception of annular fins for which the heat
+        transfer was computed explicitly.
+        """
+
+        return self.getEfficiency()*(self._area_fin/self._area_base)
+
+    def getEfficiency(self):
+        raise NotImplementedError(
+                "You must implement a getEfficiency() down the derived class."
+            )
+
+class FinPlanarRectangular(FinUniformAtr):
 
     def __init__(
             self,
@@ -337,7 +369,7 @@ class FinRectangularPlanar(FinUniformAtr):
         self._m = self._mCoeffFin()
         self._M = self._MCoeffFin()
 
-class FinRectangularPiniform(FinUniformAtr):
+class FinPiniformRectangular(FinUniformAtr):
 
     def __init__(
             self,
@@ -376,6 +408,12 @@ class FinRectangularPiniform(FinUniformAtr):
         # Their computattion depends on the geometry
         self._m = self._mCoeffFin()
         self._M = self._MCoeffFin()
+
+    def getProfileArea(self):
+
+        raise NotImplementedError(
+                  "Profile area undefined for piniform fins."
+              )
 
 class FinAnnular(FinNonUniformAtr):
 
@@ -470,13 +508,13 @@ class FinAnnular(FinNonUniformAtr):
 
         return self._Tinf + thetaRatio*self._thetaB
 
-    def getHeatTransfer(self):
-        """Return fin's total return transfer."""
+    def getEfficiency(self):
+        """Return fin's efficiency."""
 
-        # By using the equivalent length
-        # the heat transfer can be approximated
-        # by the equation for the adiabatic tip
-        return 2*np.pi*self._k*self._ri*self._t*self._thetaB*self._m*(
+        # Explicit computation based on solution provided in the text
+        c2 = (2*self._ri/self._m)/(self._rec**2 - self._ri**2)
+
+        return c2*(
                      k1(self._m*self._ri)*i1(self._m*self._rec)
                    - i1(self._m*self._ri)*k1(self._m*self._rec)
                )/(
@@ -484,7 +522,7 @@ class FinAnnular(FinNonUniformAtr):
                    + i0(self._m*self._ri)*k1(self._m*self._rec)
                )
 
-class FinTriangularPlanar(FinNonUniformAtr):
+class FinPlanarTriangular(FinNonUniformAtr):
 
     def __init__(
             self,
@@ -538,21 +576,16 @@ class FinTriangularPlanar(FinNonUniformAtr):
                       "its length."
                   )
 
-    def _thickness_x(self, x):
+    def _thickness(self, x):
+        self._check_x(x)
 
         return self._t*(1.0 - x/self._L)
 
     def _perimeter(self, x):
-
-        self._check_x(x)
-
-        return 2*self._w + 2*self._thickness_x(x)
+        return 2*self._w + 2*self._thickness(x)
 
     def _area_tr(self, x):
-
-        self._check_x(x)
-
-        return self._w*self._thickness_x(x)
+        return self._w*self._thickness(x)
 
     def getTemperature(
             self,
@@ -562,11 +595,6 @@ class FinTriangularPlanar(FinNonUniformAtr):
                   "Temperature field not implemented for triangular fin."
               )
 
-    def getHeatTransfer(self):
-        raise NotImplementedError(
-                  "Heat transfer computation not implemented for triangular fin."
-              )
-
     def getEfficiency(self):
         """Compute efficiency of triangular planar fin."""
 
@@ -574,12 +602,7 @@ class FinTriangularPlanar(FinNonUniformAtr):
                     i1(2*self._m*self._L)/i0(2*self._m*self._L)
                 )
 
-    def getEffectiveness(self):
-        raise NotImplementedError(
-                  "Effectiveness computation not implemented for triangular fin."
-              )
-
-class FinParabolicPlanar(FinNonUniformAtr):
+class FinPlanarParabolic(FinNonUniformAtr):
 
     def __init__(
             self,
@@ -640,21 +663,16 @@ class FinParabolicPlanar(FinNonUniformAtr):
                     " length."
                   )
 
-    def _thickness_x(self, x):
+    def _thickness(self, x):
+        self._check_x(x)
 
         return self._t*(1.0 - x/self._L)**2
 
     def _perimeter(self, x):
-
-        self._check_x(x)
-
-        return 2*self._w + 2*self._thickness_x(x)
+        return 2*self._w + 2*self._thickness(x)
 
     def _area_tr(self, x):
-
-        self._check_x(x)
-
-        return self._w*self._thickness_x(x)
+        return self._w*self._thickness(x)
 
     def getTemperature(
             self,
@@ -664,18 +682,174 @@ class FinParabolicPlanar(FinNonUniformAtr):
                   "Temperature field not implemented for parabolic fin."
               )
 
-    def getHeatTransfer(self):
-        raise NotImplementedError(
-                "Heat transfer computation not implemented for parabolic "\
-                "fin."
-              )
-
     def getEfficiency(self):
         """Compute efficiency of parabolic planar fin."""
 
         return 2.0/(1.0 + np.sqrt(4.0*(self._m*self._L)**2 + 1.0))
 
-    def getEffectiveness(self):
+class FinPiniformTriangular(FinNonUniformAtr):
+
+    def __init__(
+            self,
+            length: float,
+            diameter: float,
+            htc: float,
+            k: float,
+            Tinf: float,
+            Tbase: float
+        ):
+        """Initiate geometric model of a piniform triangular fin."""
+
+        FinNonUniformAtr.__init__(
+            self,
+            htc=htc,
+            k=k,
+            Tbase=Tbase,
+            Tinf=Tinf
+        )
+
+        # Triangular piniform fin test
+        self._L = length
+        self._D = diameter
+
+        self._area_fin  = 0.5*np.pi*self._D*np.sqrt(
+                                  self._L**2
+                              +   0.25*self._D**2
+                          )
+
+        self._area_base = 0.25*np.pi*self._D**2
+        self._volume = (1.0/3.0)*self._area_base*self._L
+
+        # Their computattion depends on the geometry
+        # These two have to be defined here
+        self._m = self._mCoeffFin()
+
+    def _mCoeffFin(self):
+        return np.sqrt(
+                   4.0*self._h/(self._k*self._D)
+               )
+
+    def _check_x(self, x):
+
+        if x < 0.0 or x > self._L:
+            raise ValueError(
+                      "Fin longitudinal position, x, must be smaller than "\
+                      "its length."
+                  )
+
+    def _section_diameter(self, x):
+        self._check_x(x)
+
+        return self._D*(1.0 - x/self._L)
+
+    def _perimeter(self, x):
+        return np.pi*self._section_diameter(x)
+
+    def _area_tr(self, x):
+        return 0.25*np.pi*self._section_diameter(x)**2
+
+    def getProfileArea(self):
+
         raise NotImplementedError(
-                  "Effectiveness computation not implemented for parabolic fin."
+                  "Profile area undefined for piniform fins."
               )
+
+    def getTemperature(
+            self,
+            x
+        ):
+        raise NotImplementedError(
+                  "Temperature field not implemented for triangular fin."
+              )
+
+    def getEfficiency(self):
+        """Compute efficiency of triangular piniform fin."""
+
+        return (2.0/(self._m*self._L))*(
+                    i2(2*self._m*self._L)/i1(2*self._m*self._L)
+                )
+
+class FinPiniformParabolic(FinNonUniformAtr):
+
+    def __init__(
+            self,
+            length: float,
+            diameter: float,
+            htc: float,
+            k: float,
+            Tinf: float,
+            Tbase: float
+        ):
+        """Initiate geometric model of a piniform parabolic fin."""
+
+        FinNonUniformAtr.__init__(
+            self,
+            htc=htc,
+            k=k,
+            Tbase=Tbase,
+            Tinf=Tinf
+        )
+
+        # Triangular piniform fin test
+        self._L = length
+        self._D = diameter
+
+        c3 = 1.0 + 2*(self._D/self._L)**2
+        c4 = np.sqrt(1.0 + (self._D/self._L)**2)
+
+        self._area_fin  = (np.pi*(self._L**3)/(8*self._D))*(
+                                c3*c4
+                            -   self._L/(2*self._D)*np.log(
+                                    c3 + 2*self._D*c4/self._L
+                                )
+                          )
+
+        self._area_base = 0.25*np.pi*self._D**2
+        self._volume = (1.0/5.0)*self._area_base*self._L
+
+        # Their computattion depends on the geometry
+        # These two have to be defined here
+        self._m = self._mCoeffFin()
+
+    def _mCoeffFin(self):
+        return np.sqrt(
+                   4.0*self._h/(self._k*self._D)
+               )
+
+    def _check_x(self, x):
+
+        if x < 0.0 or x > self._L:
+            raise ValueError(
+                      "Fin longitudinal position, x, must be smaller than "\
+                      "its length."
+                  )
+
+    def _section_diameter(self, x):
+        self._check_x(x)
+
+        return self._D*(1.0 - x/self._L)**2
+
+    def _perimeter(self, x):
+        return np.pi*self._section_diameter(x)
+
+    def _area_tr(self, x):
+        return 0.25*np.pi*self._section_diameter(x)**2
+
+    def getProfileArea(self):
+
+        raise NotImplementedError(
+                  "Profile area undefined for piniform fins."
+              )
+
+    def getTemperature(
+            self,
+            x
+        ):
+        raise NotImplementedError(
+                  "Temperature field not implemented for triangular fin."
+              )
+
+    def getEfficiency(self):
+        """Compute efficiency of parabolic piniform fin."""
+
+        return 2.0/(1.0 + np.sqrt((4.0/9.0)*(self._m*self._L)**2 + 1.0))
